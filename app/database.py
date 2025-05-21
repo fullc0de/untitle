aeeef97f2bd92cfd010ca0ef009ea8f536f5ec5c
Heath Hwang <heath.supergene@gmail.com>
2025-05-21 16:47:50 +0900

    turn off database log

--- app/database.py
+++ app/database.py
@@ -8,15 +8,16 @@ load_dotenv()
 # DEBUG 환경변수 확인
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-# DEBUG가 True일 때만 로깅 설정
-if DEBUG:
-    logging.basicConfig()
-    logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+# # DEBUG가 True일 때만 로깅 설정
+# if DEBUG:
+#     logging.basicConfig()
+#     logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/annyong_db")
 
 # DEBUG가 True일 때만 echo=True 설정
-engine = create_engine(DATABASE_URL, pool_size=5, echo=DEBUG)
+# engine = create_engine(DATABASE_URL, pool_size=5, echo=DEBUG)
+engine = create_engine(DATABASE_URL, pool_size=5)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
